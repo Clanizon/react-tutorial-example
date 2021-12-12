2a715ce94715e61bf811b89f46f89bc812693603
aswathpandiyan <aswathpandiyan@gmail.com>
2021-12-12 23:31:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from "react";
 import "shards-ui/dist/css/shards.min.css";
 import "./App.css";
 import CartExample from "./pages/CartExample";
 
 function App() {
-  const [count, setCount] = useState(0);
-  //var count = 0;
-  useEffect(() => {
-    console.log("component loaded");
-  }, []);
-
   return (
-    <div className="App">
+    <div>
       <CartExample />
     </div>
   );
